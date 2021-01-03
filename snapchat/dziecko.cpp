50d1099edc3615889b767a867c036ea8c69b20b0
domi131domi <d.szaci@gmail.com>
2021-01-03 13:34:31 +0100

    przyklad dzialajacego rodzica->dziecka

--- snapchat/dziecko.cpp
+++ snapchat/dziecko.cpp
@@ -9,5 +9,6 @@ int main( int argc, char** argv ) {
     std::cout<<"Jestę dzieckię"<<std::endl;
 
     std::cout<<"Dziecko zwija"<<std::endl;
+
     return 0;
 }
--- snapchat/main.cpp
+++ snapchat/main.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/wait.h> 
 
 using namespace cv;
 using namespace std;
@@ -21,31 +22,56 @@ int main( int argc, char** argv ) {
     std::cout<<"Tworze dziecko:"<<std::endl;
     char* prog = (char*)"./dziecko";
     char *args[]={prog,NULL};
-    execv(args[0],args);
 
+    pid_t child_pid,wpid;
+    int status = 0;
+
+    child_pid = fork();
+
+    if(child_pid == 0)    //dziecko
+    {
+        execv(args[0],args);
+
+        exit(1);
+    }
+    else if(child_pid == -1)
+    {
+        std::cout << "Wystapil blad tworzenia dziecka." << std::endl;
+    }
+    else
+    {
+        std::cout << "siema jestem rodzicem" << std::endl;
+    }
+
+    cout<<"Czekam na dzieci"<<std::endl;
+    while (wait(NULL) > 0);
     cout<<"Rodzic zwija"<<std::endl;
+
     return 0;
 }
 
-/*  cv::Mat image;
 
-  VideoCapture cap(0);
-    if(!cap.isOpened())
-        return -1;
+
+/*    cv::Mat image;
+
+VideoCapture cap(0);
+if(!cap.isOpened())
+return -1;
 
 int key;
+//exit aby wyjsc
 while(key != 27)
 {
-  cap >> image;
+    cap >> image;
 
-  if( !image.data ) {
-      std::cout <<  "Could not load camera view." << std::endl ;
-      return -1;
+    if( !image.data ) {
+        std::cout <<  "Could not load camera view." << std::endl ;
+        return -1;
     }
 
-  cv::imshow("Snapchat", image);
-  key = waitKey(10);
+    cv::imshow("Snapchat", image);
+    key = waitKey(10);
 }
 
-  destroyAllWindows();
- */
+destroyAllWindows();
+*/
